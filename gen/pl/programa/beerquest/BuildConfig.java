941d285f021216c86037d3c638208cb3e17b7a4e
rzaru <zarnecki@programa.pl>
2014-01-17 18:11:30 +0100

    logout g+

---
+++ gen/pl/programa/beerquest/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package pl.programa.beerquest;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
--- src/pl/programa/beerquest/interfaces/ActionBarActivity.java
+++ src/pl/programa/beerquest/interfaces/ActionBarActivity.java
@@ -1,16 +1,13 @@
 package pl.programa.beerquest.interfaces;
 
-import pl.programa.beerquest.LoginActivity;
 import pl.programa.beerquest.app.App;
 import pl.programa.beerquest.R;
-import android.content.Intent;
+import android.app.Fragment;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.ProgressBar;
-import android.widget.TextView;
-import android.widget.Toast;
 
 /**
  * Custom Activity with custom action bar and useful methods
@@ -18,61 +15,20 @@ import android.widget.Toast;
  * @author Programa.pl
  * 
  */
-public abstract class ActionBarActivity extends FragmentActivity{
+public abstract class ActionBarActivity extends Fragment{
 
 	///parameters
-	Button actionBarLogout;
-	TextView actionBarTitle;
-	ProgressBar spinner;
+	Button actionBarBack;
 	
-	Toast toast;	
 	
-
-
-	/**
-	 * onCreate
-	 * @param savedInstanceState
-	 */
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		
-
-		actionBarLogout = (Button) findViewById(R.id.actionBarLogout);
-		actionBarTitle = (TextView) findViewById(R.id.actionBarTitle);
-
-		
-		actionBarLogout.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				App.logv("logout clicked");
-				App.logoutClose(ActionBarActivity.this);
-				Intent intent = new Intent(ActionBarActivity.this, LoginActivity.class);
-				startActivity(intent);
-				finish();
-			}
-		});	
-	}
-
-	/**
-	 * set activity's custom bar title
-	 * 
-	 * @param title
-	 */
-	public void setCustomTitle(String title) {
-		App.logv("set TItle: " + title);
-		actionBarTitle = (TextView) findViewById(R.id.actionBarTitle);
-		actionBarTitle.setText(title);
-	}
-
-
-	/**
-	 * Get App singleton
-	 * 
-	 * @return App
-	 */
-	protected App getApp() {
-		return (App) getApplicationContext();
-	}
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, 
+        Bundle savedInstanceState) {
+    	App.logv("inflating action bar");
+    	View view = inflater.inflate(R.layout.action_bar, container, false);
+    	
+    	
+        return view;
+    }	
 
 }
