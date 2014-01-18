d014fe249c611336296b1cde6bfeeee3c1b55a8f
rzaru <zarnecki@programa.pl>
2014-01-18 08:53:03 +0100

    comit listy

--- gen/pl/programa/beerquest/R.java
+++ gen/pl/programa/beerquest/R.java
@@ -195,28 +195,31 @@ containing a value of this type.
         public static final int actionBar=0x7f060005;
         public static final int actionBarLogout=0x7f060006;
         public static final int actionBarNewQuest=0x7f060007;
-        public static final int action_settings=0x7f060018;
-        public static final int alertTitle=0x7f060013;
+        public static final int action_settings=0x7f06001b;
+        public static final int alertTitle=0x7f060016;
         public static final int apitest_button=0x7f06000c;
-        public static final int buttonNewQuest=0x7f06000e;
-        public static final int contentPanel=0x7f060015;
-        public static final int customPanel=0x7f060017;
+        public static final int buttonNewQuest=0x7f06000f;
+        public static final int contentPanel=0x7f060018;
+        public static final int customPanel=0x7f06001a;
         public static final int hybrid=0x7f060004;
-        public static final int icon=0x7f060012;
+        public static final int icon=0x7f060015;
+        public static final int listview=0x7f06000e;
         public static final int login_button=0x7f06000d;
         public static final int login_form=0x7f06000a;
         public static final int login_status=0x7f060008;
         public static final int login_status_message=0x7f060009;
         public static final int logo=0x7f06000b;
-        public static final int message=0x7f060016;
+        public static final int message=0x7f060019;
         public static final int none=0x7f060000;
         public static final int normal=0x7f060001;
-        public static final int parentPanel=0x7f06000f;
+        public static final int parentPanel=0x7f060012;
+        public static final int quest_item_icon=0x7f060010;
         public static final int satellite=0x7f060002;
+        public static final int secondLine=0x7f060011;
         public static final int terrain=0x7f060003;
-        public static final int titleDivider=0x7f060014;
-        public static final int title_template=0x7f060011;
-        public static final int topPanel=0x7f060010;
+        public static final int titleDivider=0x7f060017;
+        public static final int title_template=0x7f060014;
+        public static final int topPanel=0x7f060013;
     }
     public static final class integer {
         public static final int google_play_services_version=0x7f090000;
@@ -226,7 +229,8 @@ containing a value of this type.
         public static final int activity_login=0x7f030001;
         public static final int activity_main=0x7f030002;
         public static final int activity_new_quest=0x7f030003;
-        public static final int qustom_dialog_layout=0x7f030004;
+        public static final int quest_list_item=0x7f030004;
+        public static final int qustom_dialog_layout=0x7f030005;
     }
     public static final class menu {
         public static final int main=0x7f0a0000;
--- src/pl/programa/beerquest/MainActivity.java
+++ src/pl/programa/beerquest/MainActivity.java
@@ -1,5 +1,8 @@
 package pl.programa.beerquest;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -27,7 +30,10 @@ import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
@@ -114,8 +120,60 @@ public class MainActivity extends Activity {
 		final Timer myTimer = new Timer();
 		myTimer.schedule(myTask, App.MILISEC_MAP_REFRESH,
 				App.MILISEC_MAP_REFRESH);
+		
+		///LIST
+		 final ListView listview = (ListView) findViewById(R.id.listview);
+		    String[] values = new String[] { "Android", "iPhone", "WindowsMobile",
+		        "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
+		        "Linux", "OS/2", "Ubuntu", "Windows7", "Max OS X", "Linux",
+		        "OS/2", "Ubuntu", "Windows7", "Max OS X", "Linux", "OS/2",
+		        "Android", "iPhone", "WindowsMobile" };
+
+		    final ArrayList<String> list = new ArrayList<String>();
+		    for (int i = 0; i < values.length; ++i) {
+		      list.add(values[i]);
+		    }
+		    final StableArrayAdapter adapter = new StableArrayAdapter(this,
+		        android.R.layout.simple_list_item_1, list);
+		    listview.setAdapter(adapter);
+
+		    listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+		      @Override
+		      public void onItemClick(AdapterView<?> parent, final View view,
+		          int position, long id) {
+		    	  //do sth onClick
+		      }
+
+		    });
 
 	}
+	
+	  private class StableArrayAdapter extends ArrayAdapter<String> {
+
+		    HashMap<String, Integer> mIdMap = new HashMap<String, Integer>();
+
+		    public StableArrayAdapter(Context context, int textViewResourceId,
+		        List<String> objects) {
+		      super(context, textViewResourceId, objects);
+		      for (int i = 0; i < objects.size(); ++i) {
+		        mIdMap.put(objects.get(i), i);
+		      }
+		    }
+
+		    @Override
+		    public long getItemId(int position) {
+		      String item = getItem(position);
+		      return mIdMap.get(item);
+		    }
+
+		    @Override
+		    public boolean hasStableIds() {
+		      return true;
+		    }
+
+		  }
+
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
